333c28ab55d891ad2cf585e2a639afa90b335776
julien <julien@localhost>
2009-04-02 17:23:16 -0500

    file to works

---
+++ test2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rlcompleter
+import sys
+
+completer = rlcompleter.Completer()
+
+for phrase in "co", "sys.p", "is":
+    print phrase, "=>",
+    # emulate readline completion handler
+    try:
+        for index in xrange(sys.maxint):
+            term = completer.complete(phrase, index)
+            if term is None:
+                break
+            print term,
+    except:
+        pass
+    print
